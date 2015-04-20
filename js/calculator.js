385c57b15091a71fc68956cba15d941fb8469a4c
Jarrett Colby <jarrettcolby@gmail.com>
2015-04-20 12:25:32 -0400

    Various bugfixes

--- js/calculator.js
+++ js/calculator.js
@@ -1,52 +1,68 @@
-// TODO: Round the answer to the number of decimal places of the most precise operand.
-// This will mitigate floating point errors that could cause 1 + 2 = 2.9999.
-// 
-// But not for division.
-
 $(function() {
   var textField = $('#calculator-text-field');
   
+  textField.focus();
+  
   var lastNum = null;
   var operator = null;
+  var resultShown = false;
+  
+  function getInput() {
+    var str = textField.val();
+    split = str.split('.');
+    return {
+      value: parseFloat(str),
+      decimals: (split.length == 1) ? 0 : split[1].length
+    };
+  }
+  
+  function makeOperator(callback) {
+    return function() {
+      lastNum = getInput();
+      operator = callback;
+      textField.val('');
+      resultShown = false;
+    }
+  }
   
   $('#numpad button[data-char]').click(function() {
     var chr = $(this).attr('data-char');
     if (chr != '.' || textField.val().indexOf('.') == -1) {
+      if (resultShown) {
+        resultShown = false;
+        textField.val('');
+      }
       textField.val(textField.val() + chr);
     }
   });
   
-  function makeOperator(callback) {
-    return function() {
-      lastNum = parseFloat(textField.val());
-      textField.val('');
-      operator = callback;
-    }
-  }
-  
   $('#btn-add').click(makeOperator(function(a, b) {
-    return a + b;
+    return (a.value + b.value)
+      .toFixed(Math.max(a.decimals, b.decimals));
   }));
   
   $('#btn-subtract').click(makeOperator(function(a, b) {
-    return a - b;
+    return (a.value - b.value)
+      .toFixed(Math.max(a.decimals, b.decimals));
   }));
   
   $('#btn-multiply').click(makeOperator(function(a, b) {
-    return a * b;
+    return (a.value * b.value)
+      .toFixed(a.decimals + b.decimals);
   }));
   
   $('#btn-divide').click(makeOperator(function(a, b) {
-    return a / b;
+    return a.value / b.value;
   }));
   
   $('#btn-evaluate').click(function() {
     if (operator && lastNum) {
-      var currentNum = parseFloat(textField.val());
+      var currentNum = getInput();
       var result = operator(lastNum, currentNum);
       textField.val(result);
       operator = null;
       lastNum = null;
+      resultShown = true;
     }
   });
   
@@ -54,5 +70,6 @@ $(function() {
     lastNum = null;
     operator = null;
     textField.val('');
+    resultShown = false;
   });
 });
